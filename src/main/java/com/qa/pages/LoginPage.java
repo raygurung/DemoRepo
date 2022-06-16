be1a68775e4d2026514cff97598e4065ce25d354
ray <rayking132@gmail.com>
2022-06-16 12:01:59 -0600

    added search page and new login method

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -2,4 +2,18 @@ package com.qa.pages;
 
 public class LoginPage {
 
+	
+	public void loginPage()
+	
+	{
+		
+		System.out.println("login Page");
+	}
+	
+public void loginset2()
+	
+	{
+		
+		System.out.println("login Page 2");
+	}
 }
